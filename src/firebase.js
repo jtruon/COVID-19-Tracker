2d4b4816b92e7ca40f1f1544106cf0498c31af28
Joey Surge <dtruong@surgetechinc.com>
2020-09-22 22:49:15 -0500

    Added COVID - 19 Tracker

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzqWodX8zYE-DLxuejlV9gH5r4Xx32Ing",
+  authDomain: "covid-19-tracker-by-joey.firebaseapp.com",
+  databaseURL: "https://covid-19-tracker-by-joey.firebaseio.com",
+  projectId: "covid-19-tracker-by-joey",
+  storageBucket: "covid-19-tracker-by-joey.appspot.com",
+  messagingSenderId: "87189573602",
+  appId: "1:87189573602:web:f3bb4d227905c063dd33bb",
+};
